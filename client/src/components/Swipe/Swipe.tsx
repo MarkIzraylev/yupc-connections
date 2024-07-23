bd7c791d46357ff260cc2164486500113d301305
Mark <markizraylev@yandex.ru>
2024-07-23 23:31:03 +0300

    feat: put swipe card into a separate component

--- client/src/components/Swipe/Swipe.tsx
+++ client/src/components/Swipe/Swipe.tsx
@@ -2,28 +2,29 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import IconButton from '@mui/material/IconButton';
 import CheckIcon from '@mui/icons-material/Check';
-import Box from '@mui/material/Box';
 import CloseIcon from '@mui/icons-material/Close';
 import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import Mikhail from '../../static/images/Mikhail.jpg';
-import Chip from '@mui/material/Chip';
 import { grey } from '@mui/material/colors';
 import Divider from '@mui/material/Divider';
-import FlagOutlinedIcon from '@mui/icons-material/FlagOutlined';
 import Tooltip from '@mui/material/Tooltip';
-import Skeleton from '@mui/material/Skeleton';
+import Box from "@mui/material/Box";
 import { useState, useRef, useEffect } from 'react';
 
+import SwipeCard from '../SwipeCard/SwipeCard';
+
+import axios from 'axios';
+
 export default function Swipe({currentPage, setCurrentPage}: {currentPage?: any, setCurrentPage?: any}) {
-    const [imageLoaded, setImageLoaded] = useState(false);
     const [dragStart, setDragStart] = useState<number>(NaN);
     const [dragStartAfterThreshold, setDragStartAfterThreshold] = useState<number>(NaN);
     const [dragCurrent, setDragCurrent] = useState<number>(NaN);
+
+    axios.get('http://127.0.0.1:8000/api/userList/')
+    .then(response => {
+        console.log(response);
+    }).catch(err => {
+        console.error(err);
+    });
 
     var blankImg = document.createElement('img');
     blankImg.src = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
@@ -93,11 +94,8 @@ export default function Swipe({currentPage, setCurrentPage}: {currentPage?: any,
     function handleDragEnd(ev: React.DragEvent<HTMLDivElement>): void {
         if (Math.abs(dragCurrent - dragStart) < diffForDecision) {
             setDragCurrent(0)
-            
-
         }
         setDragStartAfterThreshold(NaN)
-        
         
         // оставляет карточку на месте после драга
         //setDragCurrent(ev.clientX)
@@ -124,73 +122,7 @@ export default function Swipe({currentPage, setCurrentPage}: {currentPage?: any,
                     <Grid container spacing={0} style={{alignItems: "center", height: '100%', width: '100%', transition: '0.2s'}} ref={backgroundRef}>
                         <Grid item sx={{width: '100%', backgroundColor: 'none', display:'flex', justifyContent:'center'}} p={2}>
                             <div className='swipe-card' draggable="true" onDrag={handleCardDrag} onTouchMove={handleCardDrag} onDragStart={handleDragStart} onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd} onDragEnd={handleDragEnd} onDragExit={handleDragEnd} onTouchCancel={handleTouchEnd} ref={cardRef}>
-                            <Card sx={{width: '90vw', maxWidth: '570px', height: '65vh', minHeight: '370px', overflowY: 'scroll'}}>
-                                {!imageLoaded && <Skeleton variant="rectangular" height='400px' />}
-                                <CardMedia
-                                    component="img"
-                                    alt="Изображение пользователя"
-                                    height='400px'
-                                    
-                                    image={Mikhail}
-                                    sx={{
-                                        position: 'sticky',
-                                        top: 0,
-                                        opacity: imageLoaded ? '1' : '0'
-                                    }}
-                                    onLoad={prev => setImageLoaded(true)}
-                                />
-                                <CardContent sx={{position: 'relative', backdropFilter: 'blur(30px)', backgroundColor: 'rgba(0,0,0,0.2)'}}>
-                                    <Box sx={{
-                                        display: 'grid',
-                                        flexDirection: 'row',
-                                        gridTemplateColumns: '1fr auto'
-                                    }}>
-                                        <Typography mb={2} variant="h5" noWrap component="div">
-                                        Израйлев Марк Олегович
-                                        </Typography>
-                                        <IconButton size="small" sx={{height: 'fit-content'}}>
-                                            <FlagOutlinedIcon />
-                                        </IconButton>
-                                    </Box>
-                                    
-                                    <Stack mb={2} spacing={1} direction="row" useFlexGap flexWrap="wrap">
-                                        
-                                        <Chip label="3 курс" variant="outlined" />
-                                        <Chip label="Осн. корпус" variant="outlined" />
-                                        <Chip label="ОИТ" variant="outlined" />
-                                        <Chip label="Ищу дружбу" variant="outlined" color="success" />
-
-                                    </Stack>
-                                    <Typography variant="subtitle1" mb={1}>
-                                    О себе
-                                    </Typography>
-                                    <Typography variant="body2" mb={2} sx={{
-                                        /*overflow: "hidden",
-                                        textOverflow: "ellipsis",
-                                        display: "-webkit-box",
-                                        WebkitLineClamp: "2",
-                                        WebkitBoxOrient: "vertical",*/
-                                    }}>
-                                    Люблю программирование и свою кошку Мусю. Иногда участвую в олимпиадах, поэтому если вы тоже заинтересованы в подобном, смело пишите и может быть поучаствуем вместе.
-                                    </Typography>
-                                    
-                                    <Typography variant="subtitle1" mt={1} mb={1}>
-                                    Хобби
-                                    </Typography>
-                                    <Stack spacing={1} direction="row" useFlexGap flexWrap="wrap">
-                                        
-                                        <Chip label="Программирование" variant="outlined" />
-                                        <Chip label="Прогулки" variant="outlined" />
-                                        <Chip label="Чтение" variant="outlined" />
-                                        <Chip label="Прослушивание музыки" variant="outlined" />
-                                        <Chip label="Иностранные языки" variant="outlined" />
-                                        <Chip label="Веб-разработка" variant="outlined" />
-                                        <Chip label="Рукоделие" variant="outlined" />
-                                        <Chip label="Рисование" variant="outlined" />
-
-                                    </Stack>
-                                </CardContent>
-                            </Card>
+                                <SwipeCard name="марик" mainTags={['3 курс', 'Осн. корпус', 'ОИТ']} hobbiesTags={['Программирование', 'Прогулки', 'Чтение', 'Прослушивание музыки', 'Иностранные языки', 'Веб-разработка', 'Рукоделие', 'Рисование']} intentionTag='Ищу дружбу' description='Люблю программирование и свою кошку Мусю. Иногда участвую в олимпиадах, поэтому если вы тоже заинтересованы в подобном, смело пишите и может быть поучаствуем вместе.' imageSrc='https://i.pinimg.com/originals/a0/30/46/a030463405448a5d5b052f3dc0105ebe.jpg' />
                             </div>
                         </Grid>
                         <Grid item md={12} sx={{width: '100%'}}>
