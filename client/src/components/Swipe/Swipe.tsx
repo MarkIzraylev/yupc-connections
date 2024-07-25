09089e4bf072a61abbc2961e496429bb46ba4412
Mark <markizraylev@yandex.ru>
2024-07-25 15:41:10 +0300

    feat: add data fetching via API to load and swipe multiple cards

--- client/src/components/Swipe/Swipe.tsx
+++ client/src/components/Swipe/Swipe.tsx
@@ -8,7 +8,7 @@ import { grey } from '@mui/material/colors';
 import Divider from '@mui/material/Divider';
 import Tooltip from '@mui/material/Tooltip';
 import Box from "@mui/material/Box";
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect, useReducer } from 'react';
 
 import SwipeCard from '../SwipeCard/SwipeCard';
 
@@ -19,12 +19,28 @@ export default function Swipe({currentPage, setCurrentPage}: {currentPage?: any,
     const [dragStartAfterThreshold, setDragStartAfterThreshold] = useState<number>(NaN);
     const [dragCurrent, setDragCurrent] = useState<number>(NaN);
 
-    axios.get('http://127.0.0.1:8000/api/userList/')
-    .then(response => {
-        console.log(response);
-    }).catch(err => {
-        console.error(err);
-    });
+    // const reducer = (state, action) => {
+    //     switch (action.type) {
+    //         case 'LOAD_BUNCH':
+    //             ...
+    //             return ......
+    //         case 'SHOW_NEXT':
+
+    // }
+
+    interface cardObj {
+        id: number;
+        first_name: string;
+        last_name: string;
+        sur_name: string;
+        //image: string;
+        description: string;
+        is_search_friend: boolean;
+        hobbies: string[];
+    }
+    const [currentCardId, setCurrentCardId] = useState<number>(0);
+
+    
 
     var blankImg = document.createElement('img');
     blankImg.src = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
@@ -55,6 +71,7 @@ export default function Swipe({currentPage, setCurrentPage}: {currentPage?: any,
         backgroundRef.current!.style["boxShadow"] = (() => {
             if (Math.abs(dragCurrent - dragStart) >= diffForDecision) {
                 // console.log(dragCurrent, dragStart)
+                
                 return `inset ${decisionStripeWidth * (dragCurrent > dragStart ? -1 : 1)}px 0 100px -100px ${dragCurrent > dragStart ? acceptColor : rejectColor}`;
             }
             return '';
@@ -83,20 +100,73 @@ export default function Swipe({currentPage, setCurrentPage}: {currentPage?: any,
         setDragStart(ev.changedTouches[0].clientX)
         setDragCurrent(ev.changedTouches[0].clientX)
     }
+    function sendSwipe(swipedUserId: number, swipeType: boolean) {
+        console.log(`send '${swipeType ? 'accept' : 'reject'}' swipe to a user whose id = ${swipedUserId}`)
+        // post data to server
+    }
+
+    const [currentBunchOfCards, setCurrentBunchOfCards] = useState<cardObj[]>([
+        {
+            id: 1,
+            first_name: 'Имя',
+            last_name: 'Фамилия',
+            sur_name: 'Отчество',
+            description: 'Описание карточки',
+            is_search_friend: true,
+            hobbies: ['тест хобби 1', 'тест хобби 2']
+        },
+    ]);
+
+    function fetchNewBunchOfCards(): void | boolean {
+        let nextBunchOfCards: cardObj[];
+        axios.get('http://127.0.0.1:8000/api/userList/')
+        .then(response => {
+            if (response.status === 200) {
+                nextBunchOfCards = response.data.users;
+                console.log(response.data.users)
+                setCurrentBunchOfCards(response.data.users);
+            } else {
+                return false;
+            }
+        }).catch(err => {
+            console.error(err);
+            return false;
+        });
+    }
+
+    function handleCardMoveEnd() {
+        sendSwipe(currentBunchOfCards[currentCardId].id, dragCurrent > dragStart)
+        setTimeout(() => {
+            setDragStart(0)
+            setDragCurrent(0)
+            setDragStartAfterThreshold(NaN)
+            if (currentCardId === currentBunchOfCards.length - 1) {
+                // fetch new bunch of cards via API
+                
+                fetchNewBunchOfCards()
+            }
+            setCurrentCardId(prev => (prev + 1) % currentBunchOfCards.length)
+            console.log('->', currentCardId)
+        }, 300)
+    }
     function handleTouchEnd(ev: React.TouchEvent<HTMLDivElement>): void {
         if (Math.abs(dragCurrent - dragStart) < diffForDecision) {
             setDragCurrent(0)
             
+        } else {
+            handleCardMoveEnd()
+
         }
         setDragStartAfterThreshold(NaN)
-        
     }
     function handleDragEnd(ev: React.DragEvent<HTMLDivElement>): void {
         if (Math.abs(dragCurrent - dragStart) < diffForDecision) {
             setDragCurrent(0)
+
+        } else {
+            handleCardMoveEnd()
         }
         setDragStartAfterThreshold(NaN)
-        
         // оставляет карточку на месте после драга
         //setDragCurrent(ev.clientX)
     }
@@ -112,8 +182,8 @@ export default function Swipe({currentPage, setCurrentPage}: {currentPage?: any,
         }
     }, [dragCurrent])
     useEffect(() => {
-        //setCurrentPage('swipe');
-        
+        // fetching new cards at the component's mount
+        fetchNewBunchOfCards()
     }, []);
     return (
         <>
@@ -122,7 +192,7 @@ export default function Swipe({currentPage, setCurrentPage}: {currentPage?: any,
                     <Grid container spacing={0} style={{alignItems: "center", height: '100%', width: '100%', transition: '0.2s'}} ref={backgroundRef}>
                         <Grid item sx={{width: '100%', backgroundColor: 'none', display:'flex', justifyContent:'center'}} p={2}>
                             <div className='swipe-card' draggable="true" onDrag={handleCardDrag} onTouchMove={handleCardDrag} onDragStart={handleDragStart} onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd} onDragEnd={handleDragEnd} onDragExit={handleDragEnd} onTouchCancel={handleTouchEnd} ref={cardRef}>
-                                <SwipeCard name="марик" mainTags={['3 курс', 'Осн. корпус', 'ОИТ']} hobbiesTags={['Программирование', 'Прогулки', 'Чтение', 'Прослушивание музыки', 'Иностранные языки', 'Веб-разработка', 'Рукоделие', 'Рисование']} intentionTag='Ищу дружбу' description='Люблю программирование и свою кошку Мусю. Иногда участвую в олимпиадах, поэтому если вы тоже заинтересованы в подобном, смело пишите и может быть поучаствуем вместе.' imageSrc='https://i.pinimg.com/originals/a0/30/46/a030463405448a5d5b052f3dc0105ebe.jpg' />
+                                <SwipeCard name={`${currentBunchOfCards[currentCardId].last_name} ${currentBunchOfCards[currentCardId].first_name} ${currentBunchOfCards[currentCardId].sur_name ? currentBunchOfCards[currentCardId].sur_name : ''}`} mainTags={['3 курс', 'Осн. корпус', 'ОИТ']} hobbiesTags={currentBunchOfCards[currentCardId].hobbies} intentionTag={currentBunchOfCards[currentCardId].is_search_friend ? 'Ищу дружбу' : 'Ищу любовь'} description={currentBunchOfCards[currentCardId].description} imageSrc='https://i.pinimg.com/originals/a0/30/46/a030463405448a5d5b052f3dc0105ebe.jpg' />
                             </div>
                         </Grid>
                         <Grid item md={12} sx={{width: '100%'}}>
