4980337e0460712a6b27b1a7906356694c1a34e3
Malafeew Mikhail <malaf3020@yandex.ru>
2024-08-31 02:56:08 +0300

    Проверка всех АПИ на безопасность + добавление нового ЭП для личного кабинета

--- server/SwipeApp/serializer.py
+++ server/SwipeApp/serializer.py
@@ -5,32 +5,38 @@ from .models import Hobby, Swipe, User, Course, Building, Department, ComplaintL
 from django.db.models import QuerySet, Q
 
 class UserSerializerBase(serializers.Serializer):
+    """
+        Для обработки анкет
+    """
     id = serializers.IntegerField()
     first_name = serializers.CharField(max_length=100)
     last_name = serializers.CharField(max_length=100)
     image = serializers.ImageField()
     description = serializers.CharField(max_length=170)
-    course_name = serializers.SerializerMethodField(source='course_id')
-    building_name = serializers.SerializerMethodField(source='build_id')
-    department_name = serializers.SerializerMethodField(source='department_id')
+    course_name = serializers.SerializerMethodField(source='course')
+    building_name = serializers.SerializerMethodField(source='build')
+    department_name = serializers.SerializerMethodField(source='department')
     is_search_friend = serializers.BooleanField(default=True)
     is_search_love = serializers.BooleanField(default=False)
     hobbies = serializers.SerializerMethodField()
-    is_boy = serializers.BooleanField()
 
     def get_course_name(self,obj):
-        return str(Course.objects.get(id=obj.course_id))
+        return str(Course.objects.get(id=obj.get('course')))
 
     def get_building_name(self,obj):
-        return str(Building.objects.get(id=obj.building_id))
+        return str(Building.objects.get(id=obj.get('building')))
 
     def get_department_name(self,obj):
-        return str(Department.objects.get(id=obj.department_id))
+        return str(Department.objects.get(id=obj.get('department')))
 
     def get_hobbies(self,obj):
-        return list(obj.hobbies.all().values_list('name',flat=True))
+        print("здесь, да?", obj)
+        return list(obj.get('hobbies').values_list('name',flat=True))
 
 class UserSerializerMatch(UserSerializerBase):
+    """
+        Получение информации о профиле в метчах
+    """
     vk_contact = serializers.CharField(max_length=100)
     tg_contact = serializers.CharField(max_length=100)
 
@@ -39,9 +45,14 @@ class UserSerializerProfile(UserSerializerBase):
     tg_contact = serializers.CharField(max_length=100)
 
 class SwipeUserSerializer(serializers.Serializer):
+    """
+        Для свайпа анкеты
+    """
     target_user_id = serializers.IntegerField()
     is_like = serializers.BooleanField()
+
     def create(self, validated_data):
+
         # получаем объект того, кто свайпает
         swiper_user = User.objects.get(id=self.context['request'].user.id)
 
@@ -51,37 +62,50 @@ class SwipeUserSerializer(serializers.Serializer):
         # получаем статус свайпа
         swipe_is_like = validated_data.get('is_like')
 
-        # проверяем наличие свайпа свайпера
+        # проверяем, не свайпнул ли нас Уже человек, который Мы хотим свайпнуть
         object_swipe = Swipe.objects.filter(swiper=swiped_user, swiped=swiper_user)
         if not object_swipe.exists():
+            # создаем новый свайп
             new_swipe = Swipe.objects.create(swiper=swiper_user, swiped=swiped_user,swiper_is_like=swipe_is_like)
             return new_swipe
+
+        # меняем статус в свайпе пользователя, который хотел свайпнуть
         object_swipe = object_swipe[0]
         object_swipe.swiped_is_like =  swipe_is_like
         object_swipe.save()
         return object_swipe
 
 class ResetSwipeSerializer(serializers.Serializer):
+    """
+        Для разрыва метча навсегда +
+    """
+
     target_user_id = serializers.IntegerField()
     def create(self, validated_data):
-        current_user = self.context['request'].user
+        # current_user = self.context['request'].user
+        current_user = User.objects.first()
         target_user = User.objects.get(id=validated_data['target_user_id'])
-        print("от кого запрос - ", current_user)
-        print("кого отменяем - " ,target_user)
+
+        # ищем запись о том, не были ли мы случайно инициатором свайпа(метча)
         current_user_is_swiper_record = Swipe.objects.filter(swiper=current_user,swiped=target_user)
         if current_user_is_swiper_record.exists():
-            print("я был свайпером")
             cur = current_user_is_swiper_record[0]
             cur.swiper_is_like = False
             cur.save()
             return current_user_is_swiper_record
-        print("тут меня свайпали")
-        current_user_is_swiped_record = Swipe.objects.filter(swiper=target_user, swiped=current_user)[0]
-        current_user_is_swiped_record.swiped_is_like = False
-        current_user_is_swiped_record.save()
-        return current_user_is_swiped_record
+        # ищем запись о том, что мы свайпали
+        current_user_is_swiped_record = Swipe.objects.filter(swiper=target_user, swiped=current_user)
+        if current_user_is_swiped_record.exists():
+            cur = current_user_is_swiped_record[0]
+            cur.swiped_is_like = False
+            cur.save()
+            return cur
+        return None
 
 class MatchListSerializer(serializers.Serializer):
+    """
+        Для обработки анкет в метчах +
+    """
     id = serializers.IntegerField()
     last_name = serializers.CharField(max_length=100)
     first_name = serializers.CharField(max_length=100)
@@ -92,37 +116,64 @@ class TargetUserIdSerializer(serializers.Serializer):
     target_user_id = serializers.IntegerField()
 
 class ComplaintsListSerializer(serializers.Serializer):
+    """
+        Для получения  списка жалоб +
+    """
+
     id = serializers.IntegerField()
     name = serializers.CharField(max_length=50)
 
 class SendComplaintSerializer(serializers.Serializer):
-        target_user_id = serializers.IntegerField()
-        complaint_id = serializers.IntegerField() # айди жалобы
+    """
+         Для отправки жалобы +
+    """
 
-        def create(self, validated_data):
-            complaint_obj = ComplaintTypes.objects.get(id=validated_data['complaint_id'])
-            author_obj = self.context['request'].user
-            imposter_obj = User.objects.get(id=validated_data['target_user_id'])
-            return ComplaintList.objects.create(complaint_type= complaint_obj, author_complaint = author_obj, imposter_complaint=imposter_obj)
+    target_user_id = serializers.IntegerField()
+    complaint_id = serializers.IntegerField() # айди жалобы
+
+    def create(self, validated_data):
+        complaint_obj = ComplaintTypes.objects.get(id=validated_data['complaint_id'])
+        author_obj = self.context['request'].user
+        imposter_obj = User.objects.get(id=validated_data['target_user_id'])
+        return ComplaintList.objects.create(complaint_type= complaint_obj, author_complaint = author_obj,
+                                            imposter_complaint=imposter_obj)
 
 class HobbiesListSerializer(serializers.Serializer):
+    """
+        Для получения списка хобби +
+    """
+
     id = serializers.IntegerField()
     name = serializers.CharField(max_length=50)
 
 class DepartmentsListSerializer(serializers.Serializer):
+    """
+        Для получения списка отделений +
+    """
+
     id = serializers.IntegerField()
     name = serializers.CharField(max_length=50)
 
 class BuildingsListSerializer(serializers.Serializer):
+    """
+        Для получения списка корпусов +
+    """
     id = serializers.IntegerField()
     name = serializers.CharField(max_length=50)
 
 class CoursesListSerializer(serializers.Serializer):
+    """
+        Для получение списка курсов +
+    """
     id = serializers.IntegerField()
     name = serializers.CharField(max_length=50)
 
 
 class UserFullData(serializers.Serializer):
+    """
+        Для создания нового аккаунта
+    """
+
     username = serializers.CharField(max_length=100)
     email = serializers.CharField(max_length=100)
     first_name = serializers.CharField(max_length=100)
@@ -137,8 +188,8 @@ class UserFullData(serializers.Serializer):
     tg_contact = serializers.CharField(max_length=100, required=False)
     hobbies = serializers.ListField(child=serializers.IntegerField(), write_only=True)
     password = serializers.CharField(max_length=150)
-    invitation_code = serializers.UUIDField()
     image = serializers.ImageField(required=True)
+    invitation_code = serializers.UUIDField()
 
     def create(self, validated_data):
         new_user = User(
@@ -163,5 +214,16 @@ class UserFullData(serializers.Serializer):
             hobby = Hobby.objects.get(id=hobby_id)
             new_user.hobbies.add(hobby)
         return new_user
+
+class UserDataForPersonalAccount(UserSerializerBase):
+    """
+        Получение данных личного кабинета +
+    """
+    username = serializers.CharField(max_length=200)
+    password = serializers.CharField(max_length=200)
+    email = serializers.CharField(max_length=200)
+    vk_contact = serializers.CharField(max_length=100, required=False)
+    tg_contact = serializers.CharField(max_length=100, required=False)
+
 class InvitationUserSerializer(serializers.Serializer):
     code = serializers.UUIDField()
--- server/SwipeApp/urls.py
+++ server/SwipeApp/urls.py
@@ -5,27 +5,28 @@ from django.urls import path
 
 from .views import ( UsersAPIView, SwipeAPIView, IncomingProfilesAPIView, GetMatchAPIView, GetProfileDetailsAPIView,
     ComplaintsListAPIView, SendComplaintAPIView, RegistrationAPIView, LoginAPIView, LogoutAPIView, HobbiesListAPIView,
-                     DepartmentsListAPIView, CoursesListAPIView,  BuildingListAPIView, ResetSwipeAPIView, InvitationAPIView)
+                     DepartmentsListAPIView, CoursesListAPIView,  BuildingListAPIView, ResetSwipeAPIView,
+                     InvitationAPIView, PersonalAccount)
 
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('userList/', UsersAPIView.as_view()),
-    path('swipeUser/',SwipeAPIView.as_view()),
-    path('incomingProfiles/', IncomingProfilesAPIView.as_view()),
-    path('getMatch/', GetMatchAPIView.as_view()),
-    path('getDetailsAboutProfileInMatch/', GetProfileDetailsAPIView.as_view()),
-    # path('getProfileData', )
-    path('getHobbies/', HobbiesListAPIView.as_view()),
-    path('getDepartments/', DepartmentsListAPIView.as_view()),
-    path('getBuildings/', BuildingListAPIView.as_view()),
-    path('getCourses/', CoursesListAPIView.as_view()),
-    path('getComplaints/',ComplaintsListAPIView.as_view()),
-    path('sendReport/', SendComplaintAPIView.as_view()),
-    path('registration/',  RegistrationAPIView.as_view()),
-    path('resetMatch/', ResetSwipeAPIView.as_view()),
-    path('login/', LoginAPIView.as_view()),
-    path('logout/', LogoutAPIView.as_view()),
-    path('token/refresh/',   TokenRefreshView.as_view(), name="token_refresh"),
-    path('checkInvitation/', InvitationAPIView.as_view() )
+    path('userList/', UsersAPIView.as_view()), # +
+    path('swipeUser/',SwipeAPIView.as_view()), # +
+    path('incomingProfiles/', IncomingProfilesAPIView.as_view()), # +
+    path('getMatch/', GetMatchAPIView.as_view()), # +
+    path('getDetailsAboutProfileInMatch/', GetProfileDetailsAPIView.as_view()), # +
+    path('getProfileData/',PersonalAccount.as_view()), # +
+    path('getHobbies/', HobbiesListAPIView.as_view()), # +
+    path('getDepartments/', DepartmentsListAPIView.as_view()), # +
+    path('getBuildings/', BuildingListAPIView.as_view()), # +
+    path('getCourses/', CoursesListAPIView.as_view()), # +
+    path('getComplaints/',ComplaintsListAPIView.as_view()), # +
+    path('sendReport/', SendComplaintAPIView.as_view()), # +
+    path('registration/',  RegistrationAPIView.as_view()), # +
+    path('resetMatch/', ResetSwipeAPIView.as_view()), # +
+    path('login/', LoginAPIView.as_view()), # +
+    path('logout/', LogoutAPIView.as_view()), # +
+    path('token/refresh/',   TokenRefreshView.as_view(), name="token_refresh"), # +
+    path('checkInvitation/', InvitationAPIView.as_view() ) # +
 ]
